5b79e81ea70b5440c096a1c684d5009af1e014f3
ironmaniiith <aalekhj2507@gmail.com>
2016-03-12 19:42:17 +0000

    using index as property instead of function

--- main.js
+++ main.js
@@ -2,154 +2,152 @@
 var orgs = document.getElementsByClassName('org__logo');
 
 var GLOBALS = {
-    idea: 0,
-    tech: 0,
-    img: 0
+	idea: 0,
+	tech: 0,
+	img: 0
 }
 
 /*
-    TODO
-    Make peoper use of inheritance, it's really awesome :D
-    Who said JavaScript is not Object Oriented Language :P
+	TODO
 */
 
 function Scrapers(name){
-    /*
-    Usage example:
-        var orgNames = new Scrapers(OrgNames);
-                    OR EVEN
-        var orgNames = Scrapers(OrgNames);
-    */
-    if(name){
-        return new name();
-    }
+	/*
+	Usage example:
+	var orgNames = new Scrapers(OrgNames);
+				OR
+	var orgNames = Scrapers(OrgNames);
+	*/
+	if(name){
+		return new name();
+	}
 }
 
 Scrapers.prototype.print = function(joiner, cb) {
-    if(!joiner){
-        for (var i = 0; i < this.collection.length; i++) {
-            console.log(this.collection[i]);
-        }
-    }
-    else{
-        for (var i = 0; i < this.collection.length; i++) {
-            console.log(this.collection[i].join(joiner));
-        }
-    }
-    if(cb){
-        cb(this.collection);
-    }
+	if(!joiner){
+		for (var i = 0; i < this.collection.length; i++) {
+			console.log(this.collection[i]);
+		}
+	}
+	else{
+		for (var i = 0; i < this.collection.length; i++) {
+			console.log(this.collection[i].join(joiner));
+		}
+	}
+	if(cb){
+		cb(this.collection);
+	}
 };
 
-/* Change it to property instead of a function */
-Scrapers.prototype.index = function(){
-    return this.collection.length;
-}
+Object.defineProperty(Scrapers.prototype, 'index', {
+	get: function(){
+		return this.collection.length;
+	}
+});
 
 function OrgNames() {
-    "use strict";
+	"use strict";
 
-    var _this = this;
+	var _this = this;
 
-    _this.collection = [];
+	_this.collection = [];
 
-    _this.extract = function(cb){
-        var a = document.getElementsByClassName('organization-card__container flex-sm-100 flex-md-50 flex-33');
-        for (var i = _this.index(); i < a.length; i++) {
-            var text = a[i].getAttribute('aria-label').replace('Show details for ', '');
-            console.log("Pushing");
-            _this.collection.push(text.trim());
-        }
-        console.log("Done extracting");
-        if(cb){cb(_this.collection);};
-    }
-    /* Save this in orgs.txt */
+	_this.extract = function(cb){
+		var a = document.getElementsByClassName('organization-card__container flex-sm-100 flex-md-50 flex-33');
+		for (var i = _this.index; i < a.length; i++) {
+			var text = a[i].getAttribute('aria-label').replace('Show details for ', '');
+			_this.collection.push(text.trim());
+		}
+		console.log("Done extracting");
+		if(cb){cb(_this.collection);};
+	}
+	/* Save this in orgs.txt */
 }
 OrgNames.prototype = new Scrapers();
 OrgNames.prototype.constructor = OrgNames;
 
 function IdeasPage(){
-    "use strict";
-    
-    var _this = this;
+	"use strict";
 
-    _this.collection = [];
+	var _this = this;
 
-    _this.extract = function(cb){
-        var i = _this.index();
-        GLOBALS.idea = setInterval(function(){
-            if(i >= orgs.length){
-                clearInterval(GLOBALS.idea);
-                console.log("Done extracting");
-                if(cb){cb(_this.collection);};
-            }
-            else{
-                orgs[i].click();
-                var b = document.getElementsByClassName('md-primary md-button md-soc-theme md-ink-ripple');
-                _this.collection.push(b[0].href.trim());
-            }
-            i++;
-        }, 75);
-    }
-    /* Save this in ideas.txt */
+	_this.collection = [];
+
+	_this.extract = function(cb){
+		var i = _this.index;
+		GLOBALS.idea = setInterval(function(){
+			if(i >= orgs.length){
+				clearInterval(GLOBALS.idea);
+				console.log("Done extracting");
+				if(cb){cb(_this.collection);};
+			}
+			else{
+				orgs[i].click();
+				var b = document.getElementsByClassName('md-primary md-button md-soc-theme md-ink-ripple');
+				_this.collection.push(b[0].href.trim());
+			}
+			i++;
+		}, 75);
+	}
+	/* Save this in ideas.txt */
 }
 IdeasPage.prototype = new Scrapers();
 IdeasPage.prototype.constructor = IdeasPage;
 
 function Technologies(){
-    "use strict";
-    
-    var _this = this;
+	"use strict";
 
-    _this.collection = [];
+	var _this = this;
 
-    _this.extract = function(cb){
-        var i = _this.collection.length;
-        GLOBALS.tech = setInterval(function(){
-            if (i >= orgs.length) {
-                clearInterval(GLOBALS.tech);
-                console.log("Done extracting");
-                if(cb){cb(_this.collection);};
-            }
-            else{
-                orgs[i].click();
-                _this.collection.push([]);
-                var c = document.getElementsByClassName('organization__tag organization__tag--technology');
-                for (var j = 0; j < c.length; j++) {
-                    _this.collection[i].push(c[j].innerHTML.trim());
-                }
-            }
-            i++;
-        }, 75);
-    }
-    /* Save this in technologies.txt, join with ', ' while printing */
+	_this.collection = [];
+
+	_this.extract = function(cb){
+		var i = _this.collection.length;
+		GLOBALS.tech = setInterval(function(){
+			if (i >= orgs.length) {
+				clearInterval(GLOBALS.tech);
+				console.log("Done extracting");
+				if(cb){cb(_this.collection);};
+			}
+			else{
+				orgs[i].click();
+				_this.collection.push([]);
+				var c = document.getElementsByClassName('organization__tag organization__tag--technology');
+				for (var j = 0; j < c.length; j++) {
+					_this.collection[i].push(c[j].innerHTML.trim());
+				}
+			}
+			i++;
+		}, 75);
+	}
+	/* Save this in technologies.txt, join with ', ' while printing */
 }
 Technologies.prototype = new Scrapers();
 Technologies.prototype.constructor = Technologies;
 
 function ImageLinks(){
-    "use strict";
+	"use strict";
 
-    var _this = this;
-    _this.collection = [];
+	var _this = this;
+	_this.collection = [];
 
-    _this.extract = function(cb){
-        var i = _this.index();
-        GLOBALS.img = setInterval(function(){
-            if (i >= orgs.length) {
-                clearInterval(GLOBALS.img);
-                console.log("Done extracting");
-                if(cb){cb(_this.collection);};
-            }
-            else{
-                var rawImageUrl = orgs[i].style.getPropertyValue('background-image');
-                imageUrl = rawImageUrl.replace('url("', 'http:').replace('")', '');
-                _this.collection.push(imageUrl);
-            }
-            i++;
-        }, 25);
-    };
-    /* Save this in images.txt */
+	_this.extract = function(cb){
+		var i = _this.index;
+		GLOBALS.img = setInterval(function(){
+			if (i >= orgs.length) {
+				clearInterval(GLOBALS.img);
+				console.log("Done extracting");
+				if(cb){cb(_this.collection);};
+			}
+			else{
+				var rawImageUrl = orgs[i].style.getPropertyValue('background-image');
+				var imageUrl = rawImageUrl.replace('url("', 'http:').replace('")', '');
+				_this.collection.push(imageUrl);
+			}
+			i++;
+		}, 25);
+	};
+	/* Save this in images.txt */
 }
 ImageLinks.prototype = new Scrapers();
 ImageLinks.prototype.constructor = ImageLinks;
@@ -157,36 +155,36 @@ ImageLinks.prototype.constructor = ImageLinks;
 
 /* 
 At this stage of code we have
-    var t = new Scrapers(Technologies);
-    t instanceof Scrapers;
-    // true
-    t instanceof Technologies;
-    // true
+	var t = new Scrapers(Technologies);
+	t instanceof Scrapers;
+	// true
+	t instanceof Technologies;
+	// true
 */
 
 
 var scroller = function(cb){
-    var disabled = false;
+	var disabled = false;
 
-    var temp = setInterval(function(){
-        if(!disabled){
-            var loadMoreButton = document.getElementsByClassName('table-pagination__button md-button md-soc-theme md-ink-ripple')[0];
-            loadMoreButton.click();
-            setTimeout(function(){
-                try{
-                    disable = document.getElementsByClassName('table-pagination__button md-button md-soc-theme md-ink-ripple')[0].getAttribute('disabled').trim();
-                    disabled = true;
-                    console.log("Scrolling done");
-                }
-                catch(e){
-                    // disabled = false;
-                }
-            }, 2500);
-        }
-        else{
-            clearInterval(temp);
-            // window.scrollTo(0, document.body.scrollHeight);
-            if(cb){cb();};
-        }
-    }, 4000);
+	var temp = setInterval(function(){
+		if(!disabled){
+			var loadMoreButton = document.getElementsByClassName('table-pagination__button md-button md-soc-theme md-ink-ripple')[0];
+			loadMoreButton.click();
+			setTimeout(function(){
+				try{
+					disable = document.getElementsByClassName('table-pagination__button md-button md-soc-theme md-ink-ripple')[0].getAttribute('disabled').trim();
+					disabled = true;
+					console.log("Scrolling done");
+				}
+				catch(e){
+					// disabled = false;
+				}
+			}, 2500);
+		}
+		else{
+			clearInterval(temp);
+			// window.scrollTo(0, document.body.scrollHeight);
+			if(cb){cb();};
+		}
+	}, 4000);
 }
